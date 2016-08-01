9bf0b9198c8ab080e02f9bebbfd84b02553924fb
mertakinalp <mertakinalp@gmail.com>
2016-08-01 18:27:23 +0300

    create
    create

---
+++ Ayarlar.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Drawing;
+
+namespace ImajBoyutlandirici.Ayarlar
+{
+
+    public class ImajBoyutlandiriciAyarlari
+    {
+
+        private static int _Prev1Yukseklik;
+
+        public static int Prev1Yukseklik
+        {
+            get {
+                _Prev1Yukseklik = 379;
+                return _Prev1Yukseklik; }
+        }
+
+        private static int _Prev1Genislik;
+
+        public static int Prev1Genislik
+        {
+            get 
+            {
+
+                _Prev1Genislik = 576;
+                
+                return _Prev1Genislik; 
+            }
+        }
+
+        private static int _Prev2Yuseklik;
+
+        public static int Prev2Yuseklik
+        {
+            get { _Prev2Yuseklik = 600; return _Prev2Yuseklik; }
+
+        }
+
+        private static int _Prev2Genislik;
+
+        public int Prev2Genislik
+        {
+            get { _Prev2Genislik = 800; return _Prev2Genislik; }
+   
+        }
+
+
+        private static int _Thumb3Yukseklik;
+
+        public static int Thumb3Yukseklik
+        {
+            get { _Thumb3Yukseklik = 88; return _Thumb3Yukseklik; }
+        }
+
+        private static int _Thumb3Genislik;
+
+        public static int Thumb3Genislik
+        {
+            get { _Thumb3Genislik = 251; return _Thumb3Genislik; }
+
+        }
+
+        private static int _Thumb4Yuseklik;
+
+        public static int Thumb4Yuseklik
+        {
+            get { _Thumb4Yuseklik = 87; return _Thumb4Yuseklik; }
+        }
+
+        private static int _Thumb4Genislik;
+
+        public static int Thumb4Genislik
+        {
+            get { _Thumb4Genislik = 113; return _Thumb4Genislik; }
+        }
+
+        private static int _Flash1Yukseklik;
+
+        public static int Flash1Yukseklik
+        {
+            get { _Flash1Yukseklik = 260; return _Flash1Yukseklik; }
+        }
+
+
+        private static int _Flash1Genislik;
+
+        public static int Flash1Genislik
+        {
+            get { _Flash1Genislik = 560; return _Flash1Genislik; }
+        }
+
+        private static int _Flash2Yukseklik;
+
+        public static int Flash2Yukseklik
+        {
+            get { _Flash2Yukseklik = 199; return _Flash2Yukseklik; }
+        }
+
+        private static int _Flash2Genislik;
+
+        public static int Flash2Genislik
+        {
+            get { _Flash2Genislik = 550; return _Flash2Genislik; }
+        }
+
+        private static int _ListYukseklik;
+
+        public static int ListYukseklik
+        {
+            get { _ListYukseklik = 99; return _ListYukseklik; }
+        }
+
+        private static int _ListGenislik;
+
+        public static int ListGenislik
+        {
+            get { _ListGenislik = 236; return _ListGenislik; }
+        }
+
+        private static int _List2Yukseklik;
+
+        public static int List2Yukseklik
+        {
+            get { _List2Yukseklik = 101; return _List2Yukseklik; }
+        }
+
+        private static int _List2Genislik;
+
+        public static int List2Genislik
+        {
+            get { _List2Genislik = 284; return _List2Genislik; }
+        }
+
+        private static int _Thumb1Yukseklik;
+
+        public static int Thumb1Yukseklik
+        {
+            get { _Thumb1Yukseklik = 52; return _Thumb1Yukseklik; }
+        }
+
+        private static int _Thumb1Genislik;
+
+        public static int Thumb1Genislik
+        {
+            get { _Thumb1Genislik = 92; return _Thumb1Genislik; }
+        }
+
+        private static int _Thumb2Yukseklik;
+
+        public static int Thumb2Yukseklik
+        {
+            get { _Thumb2Yukseklik = 30; return _Thumb2Yukseklik; }
+        }
+
+        private static int _Thumb2Genislik;
+
+        public static int Thumb2Genislik
+        {
+            get { _Thumb2Genislik = 67; return _Thumb2Genislik; }
+        }
+
+        private string _Kaynak;
+
+        public string Kaynak
+        {
+            get { return _Kaynak; }
+            set
+            {
+                _Kaynak = value;        
+            }
+        }
+
+        private string _Hedef;
+
+        public string Hedef
+        {
+            get { return _Hedef; }
+            set { _Hedef = value; }
+        }
+
+        private int _Yukseklik;
+
+        public int Yukseklik
+        {
+            get { return _Yukseklik; }
+            set { _Yukseklik = value; }
+        }
+        private int _Genislik;
+
+        public int Genislik
+        {
+            get { return _Genislik; }
+            set { _Genislik = value; }
+        }
+       
+
+        private int _EYukseklik;
+
+        public int EYukseklik
+        {
+            get { return _EYukseklik; }
+            set { _EYukseklik = value; }
+        }
+        private int _EGenislik;
+
+        public int EGenislik
+        {
+            get { return _EGenislik; }
+            set { _EGenislik = value; }
+        }
+
+        private Size _EBoyut;
+
+        public Size EBoyut
+        {
+            get { return _EBoyut; }
+            set { _EBoyut = value; }
+        }
+
+
+
+
+        public ImajBoyutlandiriciAyarlari(int yukseklik, int genislik,string hedefdosyadizini,string hedefdosyaadi)
+        {
+            
+
+
+
+            if ( string.IsNullOrEmpty(hedefdosyadizini) || string.IsNullOrEmpty(hedefdosyaadi)) throw new Exception("Hedef dosya adı,dizini veya Kaynak dosya adı,dizini Boş Olamaz;  Hedef Dosya Adı = " + hedefdosyaadi+ " ; Hedef Dosya Dizini = " + hedefdosyadizini);
+            else if (yukseklik <= 0 || genislik <= 0) throw new Exception("Yükseklik veya Genişlik 0 yada daha küçük olamaz ; Yükseklik = " + yukseklik.ToString() + " ; Genişlik = " + genislik.ToString());
+            
+
+            
+            this.Yukseklik = yukseklik;
+            this.Genislik = genislik;
+
+
+
+
+            
+
+            if (Directory.Exists(hedefdosyadizini) == false)
+            {
+
+                try
+                {
+                    Directory.CreateDirectory(hedefdosyadizini);
+                }
+                catch (Exception Exm)
+                {
+
+                    throw new Exception("Olmayan Hedef Dosya Dizini Yaratılamadı ; " + Exm.Message);
+                }
+            }
+
+            if (File.Exists(hedefdosyadizini + hedefdosyaadi) == false)
+            {
+                try
+                {
+                   // File.Create(hedefdosyadizini + hedefdosyaadi);
+
+                  //  Image dosya = Image.FromFile(hedefdosyadizini + hedefdosyaadi);
+
+                    //dosya.Save(hedefdosyadizini + hedefdosyaadi);
+                }
+                catch (Exception exm)
+                {
+                    throw new Exception("Olmayan hedef dosya yaratılamadı ; " + exm.Message);
+                }
+
+            }
+
+            
+            this.Hedef = hedefdosyadizini + hedefdosyaadi;
+        }
+
+    }
+}
---
+++ Boyutlandirmalar.cs
@@ -0,0 +1,285 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Text;
+using System.Drawing;
+using ImajBoyutlandirici.Ayarlar;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
+using System.IO;
+
+
+namespace ImajBoyutlandirici.Boyutlandirma
+{
+    public class ImajBoyutlandirma
+    {
+
+
+        private ImajBoyutlandiriciAyarlari[] _Ayarlar;
+
+        public ImajBoyutlandiriciAyarlari[] Ayarlar
+        {
+            get { return _Ayarlar; }
+            set { _Ayarlar = value; }
+        }
+
+
+        private Image _Resim;
+
+        public Image Resim
+        {
+            get { return _Resim; }
+            set { _Resim = value; }
+        }
+        private float _EOran;
+
+        protected float EOran
+        {
+            get { return _EOran; }
+            set { _EOran = value; }
+        }
+
+        private float _Oran;
+
+        protected float Oran
+        {
+            get { return _Oran; }
+            set { _Oran = value; }
+        }
+        private Enum _kalite;
+
+        public Enum Kalite
+        {
+            get { return _kalite; }
+            set { _kalite = value; }
+        }
+
+        public enum Kaliteler
+        {
+            Bicubic,
+            Bilinear,
+            Default,
+            High,
+            HighQualityBicubic,
+            HighQualityBilinear,
+            Invalid,
+            Low,
+            NearestNeighbor
+        }
+      
+
+        private void BoyutKucultucu(ImajBoyutlandiriciAyarlari Ayarlar)
+        {
+            EOran = (float)Ayarlar.EYukseklik / (float)Ayarlar.EGenislik;
+
+            Oran = (float)Ayarlar.Yukseklik / (float)Ayarlar.Genislik;
+
+            Ayarlar.Yukseklik = (int)(Ayarlar.Genislik * EOran);
+
+            Bitmap btmp = new Bitmap(Ayarlar.Genislik, Ayarlar.Yukseklik, Resim.PixelFormat);
+
+            Graphics YeniResim = Graphics.FromImage(btmp);
+
+            YeniResim.DrawImage(Resim, 0, 0, Ayarlar.Genislik, Ayarlar.Yukseklik);
+
+            if (File.Exists(Ayarlar.Hedef) == true) { File.Delete(Ayarlar.Hedef); }
+
+            btmp.Save(Ayarlar.Hedef);
+
+            btmp.Dispose();
+            YeniResim.Dispose();
+
+
+            GC.Collect();
+        }
+
+
+
+        public ImajBoyutlandirma(ImajBoyutlandiriciAyarlari[] ayarlar, string kaynakdosyadizini, string kaynakdosyaadi, Kaliteler kalite)
+        {
+
+           kaynakdosyaadi = kaynakdosyaadi.ToLower();
+
+            if (string.IsNullOrEmpty(kaynakdosyadizini) || string.IsNullOrEmpty(kaynakdosyaadi)) throw new Exception("Kaynak Dosya dizini veya Kaynak Dosya adı boş olamaz ; Kaynak Dosya Dizini = " + kaynakdosyadizini + " ; Kaynak Dosya Adı = " + kaynakdosyaadi);
+            else if ((kaynakdosyaadi.EndsWith(".jpg") || kaynakdosyaadi.EndsWith(".gif") || kaynakdosyaadi.EndsWith(".png") || kaynakdosyaadi.EndsWith(".bmp") || kaynakdosyaadi.EndsWith(".jpeg")) == false) throw new Exception("Kaynak dosya Resim formatı Taşımalı ; Kaynak = " + kaynakdosyaadi);
+            else if (File.Exists(kaynakdosyadizini + kaynakdosyaadi) == false) throw new Exception("Kaynak gösterilen dizindeki dosya mevcut değil ; Kaynak = " + kaynakdosyadizini + kaynakdosyaadi);
+            else if (ayarlar.Length <= 0) throw new Exception("Ayarlar Elemanı en az 1 olabilir ; ayarlar eleman sayısı = " + ayarlar.Length.ToString());
+
+            this.Ayarlar = ayarlar;
+
+
+            foreach (ImajBoyutlandiriciAyarlari Ayar in this.Ayarlar)
+            {
+                Ayar.Kaynak = kaynakdosyadizini + kaynakdosyaadi;
+                
+                this.Resim = ((Image)(Bitmap.FromFile(Ayar.Kaynak).Clone()));
+                //EncoderParameter kalite = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, Ayar.Kalite);
+                
+                Ayar.EGenislik = Resim.Width;
+                
+                Ayar.EYukseklik = Resim.Height;
+                
+                Ayar.EBoyut = Resim.Size;
+                
+                Ayar.Hedef = Ayar.Hedef;
+
+
+                if (Ayar.Hedef.Contains("prev1") == true || Ayar.Hedef.Contains("prev2") == true)
+                {
+
+                   
+                    if (Resim.Width < Resim.Height)
+                    {
+
+                        Ayar.Genislik = Ayar.Yukseklik;
+
+                        Ayar.Yukseklik = Ayar.Genislik;
+                    }
+                }
+
+              
+
+                ImajKaydedici(OrtadanParcalayici(Ayar.Genislik, Ayar.Yukseklik,Kaliteler.HighQualityBilinear), Ayar.Hedef);
+                GC.Collect();
+            }
+
+        }
+        private Image OrtadanParcalayici(int GelenGenislik, int GelenYukseklik, Kaliteler GelenKalite)
+        {
+            int GelenKaynakGenislik = Resim.Width;
+
+            int GelenKaynakYukseklik = Resim.Height;
+
+            int EskiX = 0;
+
+            int EskiY = 0;
+
+            int YeniX = 0;
+
+            int YeniY = 0;
+
+            float nOranti = 0;
+
+            float nOrantiG = 0;
+
+            float nOrantiY = 0;
+
+            nOrantiG = ((float)GelenGenislik / (float)GelenKaynakGenislik);
+
+            nOrantiY = ((float)GelenYukseklik / (float)GelenKaynakYukseklik);
+
+            if (nOrantiY < nOrantiG)
+            {
+                nOranti = nOrantiG;
+
+                YeniY = (int)((GelenYukseklik - (GelenKaynakYukseklik * nOranti)) / 2);
+
+
+            }
+            else
+            {
+                nOranti = nOrantiY;
+
+                YeniX = (int)((GelenGenislik - (GelenKaynakGenislik * nOranti)) / 2);
+
+            }
+
+            int YeniGenislik = (int)(GelenKaynakGenislik * nOranti);
+
+            int YeniYukseklik = (int)(GelenKaynakYukseklik * nOranti);
+
+
+            Bitmap YeniFoto = new Bitmap(GelenGenislik, GelenYukseklik, PixelFormat.Format24bppRgb);
+
+            YeniFoto.SetResolution(Resim.HorizontalResolution, Resim.VerticalResolution);
+            
+
+            Graphics Grafik = Graphics.FromImage(YeniFoto);
+
+            switch (GelenKalite)
+            {
+                case Kaliteler.Bicubic:
+                    Grafik.InterpolationMode = InterpolationMode.Bicubic;
+                    break;
+                case Kaliteler.Bilinear:
+                    Grafik.InterpolationMode = InterpolationMode.Bilinear;
+                    break;
+                case Kaliteler.Default:
+                    Grafik.InterpolationMode = InterpolationMode.Default;
+                    break;
+                case Kaliteler.High:
+                    Grafik.InterpolationMode = InterpolationMode.High;
+                    break;
+                case Kaliteler.HighQualityBicubic:
+                    Grafik.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                    break;
+                case Kaliteler.HighQualityBilinear:
+                    Grafik.InterpolationMode = InterpolationMode.HighQualityBilinear;
+                    break;
+                case Kaliteler.Invalid:
+                    Grafik.InterpolationMode = InterpolationMode.Invalid;
+                    break;
+                case Kaliteler.Low:
+                    Grafik.InterpolationMode = InterpolationMode.Low;
+                    break;
+                case Kaliteler.NearestNeighbor:
+                    Grafik.InterpolationMode = InterpolationMode.NearestNeighbor;
+                    break;
+                default:
+                    Grafik.InterpolationMode = InterpolationMode.Default;
+                    break;
+            }
+
+ 
+            Grafik.DrawImage(Resim, new Rectangle(YeniX, YeniY, YeniGenislik, YeniYukseklik), new Rectangle(EskiX, EskiY, GelenKaynakGenislik, GelenKaynakYukseklik), GraphicsUnit.Pixel);
+
+
+            Grafik.Dispose();
+
+            GC.Collect();
+
+            return YeniFoto;
+            
+
+
+        }
+
+        public static string degisti6;
+        private void ImajKaydedici(Image KaydedilecekResim, string KaydedilecekHedef)
+        {
+
+
+            EncoderParameter qualityParam =
+      new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, (long)75 );
+            
+            ImageCodecInfo jpegCodec = GetEncoderInfo("image/jpeg");
+
+            EncoderParameters encoderParams = new EncoderParameters(1);
+            encoderParams.Param[0] = qualityParam;
+
+            
+
+            
+            
+            if (File.Exists(KaydedilecekHedef) == true) File.Delete(KaydedilecekHedef);
+            KaydedilecekResim.Save(KaydedilecekHedef,jpegCodec,encoderParams);
+            KaydedilecekResim.Dispose();
+            GC.Collect();
+
+
+        }
+        private static ImageCodecInfo GetEncoderInfo(string mimeType)
+        {
+            // Get image codecs for all image formats 
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageEncoders();
+
+            // Find the correct image codec 
+            for (int i = 0; i < codecs.Length; i++)
+                if (codecs[i].MimeType == mimeType)
+                    return codecs[i];
+            return null;
+        } 
+
+
+    }
+}
